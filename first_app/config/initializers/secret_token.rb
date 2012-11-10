0d5739c3c14e83d293bb73010faf2057ecfab71e
Kevin Sihlanick <Kevin.Sihlanick@gmail.com>
2012-11-10 15:28:56 -0500

    built default rails app and installed budles

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '76dd140417832ab7bbe2c1bed0a04d378cad1956dc996d22f83c1bde736c3f840249815d5ab9232cb4fed2168ec23590170796f024b805f993ae281e2f2e94dc'
